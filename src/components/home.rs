461870b44e3af2fb1af79f7a59fd15e74c8cde7d
Irakli Ambroladze <ambroladzeirakli@gmail.com>
2026-01-16 02:04:08 +0400

    feat(Home): add create new game button and route move

--- src/components/home.rs
+++ src/components/home.rs
@@ -1,10 +1,18 @@
 use leptos::prelude::*;
+use leptos_router::hooks::use_navigate;
 
 #[component]
 pub fn Home() -> impl IntoView {
+    let navigate = use_navigate();
+    let navigate_clone1 = navigate.clone();
+    let create_game = move |_| {
+        let code = 12;
+        navigate_clone1(&format!("/game/{}?action=create", code), Default::default());
+    };
     view! {
         <div>
             <h1>"Chess Game"</h1>
+            <button on:click=create_game>"Create New Game"</button>
         </div>
     }
 }
