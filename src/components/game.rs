10636b12d550fb6dfb69bd13de191998f7a766d0
Irakli Ambroladze <ambroladzeirakli@gmail.com>
2026-01-16 03:07:08 +0400

    feat: create draft game component

---
+++ src/components/game.rs
@@ -0,0 +1,11 @@
+use leptos::prelude::*;
+
+#[component]
+pub fn Game() -> impl IntoView {
+    view! {
+            <div>
+                <h2>"Game"</h2>
+                <p>"Game will go here"</p>
+            </div>
+    }
+}
