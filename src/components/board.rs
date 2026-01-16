e887cde2ec35e5dcb3b7f69f3e9b6eb66280ac2c
Irakli Ambroladze <ambroladzeirakli@gmail.com>
2026-01-16 17:57:56 +0400

    feat(Board): add board UI

--- src/components/board.rs
+++ src/components/board.rs
@@ -3,7 +3,26 @@ use leptos::prelude::*;
 #[component]
 pub fn Board() -> impl IntoView {
     view! {
-        <div>"I am a board"</div>
+        <div class="chess-board">
+            <For
+                each=move || {
+                    let mut squares = Vec::new();
+                    for rank in (0..8).rev() {
+                        for file in 0..8 {
+                            squares.push((file, rank));
+                        }
+                    }
+                    squares
+                }
+                key=|(f, r)| format!("{}{}", f, r)
+                children=move |(file, rank)| {
+                    let is_light = (file + rank) % 2 == 0;
+                    view! {
+                        <div class="square" class:light=is_light class:dark=!is_light> </div>
+                    }
+                }
+            />
+                </div>
     }
 }
 
